6236eee989ff279e77a9bb05e48321228c7a601f
Sebastian Cortez Apaza <sc2023078699@virtual.upt.pe>
2025-10-23 16:45:33 -0500

    Version 1

---
+++ src/java/ModeloDao/ClienteDAO.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModeloDao;
+import Config.ConnectionConfig;
+import Interfaces.CrudCliente;
+import Modelo.Cliente;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+/**
+ *
+ * @author HP
+ */
+public class ClienteDAO implements CrudCliente{
+    ConnectionConfig cn=new Conecion();/
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    Cliente c =new Cliente();
+    @Override
+    public List listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Cliente list(String dni) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Cliente cli) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean edit(Cliente cli) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(String dni) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
